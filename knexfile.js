4f944d7214d18a289246de71bf4db83cf2c8e68b
Justyna Field <justyna.field@gmail.com>
2017-08-22 14:00:51 -0600

    set up knex environments

---
+++ knexfile.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost//byob',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations',
+    },
+    seeds: {
+      directory: './db/dev/seeds'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    }
+  },
+
+  test: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/byob',
+    useNullAsDefault: true,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/test/seeds'
+    }
+  }
+};
