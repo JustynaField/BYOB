8ee4929d2dd4625629ddd8e41d2d4e597e5726f3
Justyna Field <justyna.field@gmail.com>
2017-08-23 17:25:57 -0600

    minor fixes

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/byob',
+    connection: 'postgres://localhost//byob',
     useNullAsDefault: true,
     migrations: {
       directory: './db/migrations',
