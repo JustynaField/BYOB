919b51baf1526924d33f6c126424a69de2413d3d
Justyna Field <justyna.field@gmail.com>
2017-08-25 16:36:48 -0600

    add test for beer and specific beer

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -63,7 +63,8 @@ describe('API Routes', () => {
   });
 
   describe('GET /api/v1/brewery/:id', () => {
-    it('should return a brewery by id', (done) => {
+
+    it.skip('should return a brewery by id', (done) => {
       chai.request(server)
       .get('/api/v1/brewery/1')
       .end((error, response) => {
@@ -77,6 +78,15 @@ describe('API Routes', () => {
         done();
       })
     });
+
+    it('should return an error if a requested brewery does not exist', (done) => {
+      chai.request(server)
+      .get('/api/v1/brewery/100')
+      .end((error, response) => {
+        response.should.have.status(404);
+        done();
+      })
+    })
   });
 
   describe('POST /api/v1/brewery', () => {
@@ -89,14 +99,17 @@ describe('API Routes', () => {
       .end((error, response) => {
         response.should.have.status(201);
         response.body.should.be.a('object');
-        // response.body.should.have.property('name');
-        // response.body.lastname.should.equal('New Local Brewery');
+        response.body.should.have.property('name');
+        response.body.lastname.should.equal('New Local Brewery');
         chai.request(server)
         .get('/api/v1/brewery')
         .end((error, response) => {
           response.should.have.status(200);
           response.should.be.json;
           response.body.should.be.a('array');
+          response.body.length.should.equal(31);
+          response.body[30].should.have.property('name');
+          response.body[30].name.should.equal('New Local Brewery');
           done();
         })
       });
@@ -112,13 +125,54 @@ describe('API Routes', () => {
         respons.should.have.status(422);
         response.body.error.should.equal('Missing required parameter name.');
         done();
-      })
-    })
+      });
+    });
   });
 
+  describe('GET /api/v1/beer', () => {
+    it('should return all beers', (done) => {
+      chai.request(server)
+      .get('/api/v1/beer')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(69);
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('Denver Pale Ale');
+        response.body[0].should.have.property('style');
+        response.body[0].style.should.equal('American Pale Wheat Ale(APA)');
+        response.body[0].should.have.property('size');
+        response.body[0].size.should.equal('12 oz');
+        response.body[0].should.have.property('abv');
+        response.body[0].abv.should.equal('5.0%');
+        done();
+      });
+    });
+  });
 
+  describe('GET /api/v1/beer/:id', () => {
 
+    it.skip('should return beer by id', (done) => {
+      chai.request(server)
+      .get('api/v1/beer/1')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        done();
+      });
+    });
 
+    it('should not return beer that does not exist', (done) => {
+          chai.request(server)
+          .get('/api/v1/beer/100')
+          .end((error, response) => {
+            response.should.have.status(404);
+            done();
+          })
+        })
+
+  })
 
 
 });
