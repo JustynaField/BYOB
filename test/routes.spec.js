4254f60c351933d282925e59cb1d1e704d78e9b3
Justyna Field <justyna.field@gmail.com>
2017-08-25 16:10:22 -0600

    add testing for specific brewery

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,12 +1,12 @@
-// process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test';
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
 
-// const environment = process.env.NODE_ENV;
-// const configuration = require('../knexfile')[environment];
-// const database = require('knex')(configuration);
+const environment = process.env.NODE_ENV;
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
 
 chai.use(chaiHttp);
 
@@ -33,19 +33,18 @@ describe('Client Routes', () => {
 
 describe('API Routes', () => {
 
-  // beforeEach((done) => {
-  //   database.migrate.rollback()
-  //   .then(() => {
-  //     database.migrate.latest()
-  //     .then(() => {
-  //       database.seed.run()
-  //       .then(() => {
-  //         done();
-  //       });
-  //     });
-  //   });
-  // });
-
+  beforeEach((done) => {
+    database.migrate.rollback()
+    .then(() => {
+      database.migrate.latest()
+      .then(() => {
+        database.seed.run()
+        .then(() => {
+          done();
+        });
+      });
+    });
+  });
 
   describe('GET /api/v1/brewery', () => {
     it('should return all of the breweries', (done) => {
@@ -55,11 +54,28 @@ describe('API Routes', () => {
         response.should.have.status(200);
         response.should.be.json;
         response.body.should.be.a('array');
-        // response.body.length.should.equal(30);
+        response.body.length.should.equal(30);
         response.body[0].should.have.property('name');
-        response.body[0].name.should.equal('Avery Brewing Company');
+        response.body[0].name.should.equal('Great Divide Brewing Company');
         done();
       });
+    });
+  });
+
+  describe('GET /api/v1/brewery/:id', () => {
+    it('should return a brewery by id', (done) => {
+      chai.request(server)
+      .get('/api/v1/brewery/1')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body[0].should.be.a('object');
+        response.body.length.should.equal(1);
+        response.body[0].should.have.property('name');
+        response.body[0].name.should.equal('Great Divide Brewing Company');
+        done();
+      })
     });
   });
 
