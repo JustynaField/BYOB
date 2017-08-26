dfa5bf28b3e26483b1c25ae4a2892c87236b20a7
Travis <tlgreg86@gmail.com>
2017-08-26 17:22:59 -0600

    Restructure seed file to hold minimal data and work with small data set.

--- db/seeds/test/brewery.js
+++ db/seeds/test/brewery.js
@@ -1,4 +1,68 @@
+const brewery = [
+  {
+		'id': 1,
+		'name': 'Great Divide Brewing Company'
+	},
+	{
+		'id': 2,
+		'name': 'Avery Brewing Company'
+	}
+]
 
+const beer = [
+	{
+		'id': 1,
+		'name': 'Denver Pale Ale',
+		'style': 'American Pale Wheat Ale(APA)',
+		'size': '12 oz',
+		'abv': '5.0%',
+		'brewery_id': 1
+	},
+	{
+		'id': 2,
+		'name': 'Hibernation Ale',
+		'style': 'Old Ale',
+		'size': '12 oz',
+		'abv': '8.7%',
+		'brewery_id': 1
+	},
+	{
+		'id': 3,
+		'name': 'Whitewater',
+		'style': 'American Pale Wheat Ale',
+		'size': '12 oz',
+		'abv': '6.1%',
+		'brewery_id': 2
+	},
+	{
+		'id': 4,
+		'name': 'Raja',
+		'style': 'American Double/ Imperial IPA',
+		'size': '12 oz',
+		'abv': '8.0%',
+		'brewery_id': 2
+	}
+]
 
 exports.seed = (knex, Promise) => {
+  return knex('beer').del()
+    .then(() => {
+      return knex('brewery').del();
+    })
+    .then(() => {
+      return Promise.all(brewery.map((brewery) => {
+        return knex('brewery').insert(brewery);
+      }));
+    })
+    .then(() => {
+      return Promise.all(beer.map((beer) => {
+        return knex('beer').insert(beer);
+      }));
+    })
+    .then(() => {
+      console.log('Re-seeding Complete');
+    })
+    .catch(error => {
+      console.log({error: 'Error seeding data'});
+    })
 };
