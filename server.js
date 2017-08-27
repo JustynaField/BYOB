97e810bfdfca544f1c33a72f070c820c2ee021cc
Travis <tlgreg86@gmail.com>
2017-08-27 11:32:06 -0600

    Delete unused or bad code

--- server.js
+++ server.js
@@ -163,32 +163,13 @@ app.get('/api/v1/brewery/:id/beer', (request, response) => {
     .where('brewery_id', request.params.id)
     .select()
     .then(beer => {
-      response.status(200).json(beer);
-    })
-    .catch(error => {
-      response.status(404).json({
-        error: `Could not find brewery with id of ${request.params.id}`
-      })
-    })
-})
-
-app.delete('/api/v1/brewery/:id/beer', (request, response) => {
-  database('beer')
-    .where('brewery_id', request.params.id)
-    .del('*')
-		.then(obj => {
-			if (obj.length) {
-				response.status(201).json({obj})
 			} else {
 				response.status(404).json({error: 'No breweries with this id exist'})
 			}
-		})
     .catch(error => {
       response.status(500).json({ error: 'Request cannot be processed' })
     })
-});
 
-app.post('/api/v1/brewery/:id/beer', (request, response) => {
 	const newBeer = request.body;
 
 	for(let requiredParameter of ['name', 'style', 'size', 'abv']) {
@@ -205,11 +186,8 @@ app.post('/api/v1/brewery/:id/beer', (request, response) => {
 		.then(brewery => {
 			database('beer').insert(newBeer, '*')
 				.then(beer => {
-					response.status(201).json(beer[0]);
-				});
 		})
 		.catch(error => {
-			response.status(500).json({ error });
 		});
 });
 
@@ -220,10 +198,8 @@ app.patch('/api/v1/beer/:id', (request, response) => {
 		.where('id', request.params.id)
 		.update(newData, '*')
 		.then(data => {
-			response.status(201).json(data);
 		})
 		.catch(error => {
-			console.log(error);
 			response.status(500).json({ error });
 		});
 });
@@ -234,7 +210,6 @@ app.delete('/api/v1/beer/:id', (request, response) => {
     .del('*')
 		.then(obj => {
 			if (obj.length) {
-				response.status(201).json({obj})
 			} else {
 				response.status(404).json({error: 'No beers exist with that id'})
 			}
