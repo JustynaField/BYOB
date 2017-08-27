9c684fe9604babc786b57e82f152954b92cd51af
Justyna Field <justyna.field@gmail.com>
2017-08-27 13:25:40 -0600

    style printed token

---
+++ public/script.js
@@ -0,0 +1,35 @@
+const submit = document.getElementById('submit');
+
+const authenticateUser = () => {
+  const emailVal = document.getElementById('email').value;
+  const appNameVal = document.getElementById('app-name').value;
+
+  fetch('/authentication', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      email: emailVal,
+      appName: appNameVal
+    })
+  })
+  .then(res => res.json())
+  .then(data => {
+    printToken(data.token)
+  })
+  .catch(error => console.log(error))
+}
+
+submit.addEventListener('click', () => {
+  const email = document.getElementById('email');
+  const app = document.getElementById('app-name');
+
+  authenticateUser()
+  printToken()
+  email.value = '';
+  app.value = ''
+})
+
+const printToken = (data) => {
+  const token = document.getElementById('token');
+  token.innerText = data
+}
