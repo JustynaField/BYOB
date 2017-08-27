4635b364a55a34b7f4c7ef449ad03804418e6080
Justyna Field <justyna.field@gmail.com>
2017-08-27 15:15:03 -0600

    clean FE script.js

--- public/script.js
+++ public/script.js
@@ -1,35 +1,35 @@
 const submit = document.getElementById('submit');
 
+const printToken = (data) => {
+  const token = document.getElementById('token');
+  token.innerText = data;
+};
+
 const authenticateUser = () => {
   const emailVal = document.getElementById('email').value;
   const appNameVal = document.getElementById('app-name').value;
 
   fetch('/authentication', {
     method: 'POST',
-    headers: {'Content-Type': 'application/json'},
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({
       email: emailVal,
-      appName: appNameVal
+      appName: appNameVal,
+    }),
+  })
+    .then(res => res.json())
+    .then((data) => {
+      printToken(data.token);
     })
-  })
-  .then(res => res.json())
-  .then(data => {
-    printToken(data.token)
-  })
-  .catch(error => console.log(error))
-}
+    .catch(error => console.log(error));
+};
 
 submit.addEventListener('click', () => {
   const email = document.getElementById('email');
   const app = document.getElementById('app-name');
 
-  authenticateUser()
-  printToken()
+  authenticateUser();
+  printToken();
   email.value = '';
-  app.value = ''
-})
-
-const printToken = (data) => {
-  const token = document.getElementById('token');
-  token.innerText = data
-}
+  app.value = '';
+});
